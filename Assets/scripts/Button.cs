520adfbad2e462cf57d473bee496ce744f76f2d7
Toxiiic <872435708@qq.com>
2017-12-08 16:46:30 +0800

    add lazer base in button system, which has big problem now

--- Assets/scripts/Button.cs
+++ Assets/scripts/Button.cs
@@ -4,23 +4,62 @@ using UnityEngine;
 
 public class Button : MonoBehaviour {
 	public bool AutoUp = false;
+	public float moveDuration = .2f;
 
+	/* 按钮状态，默认关 */
 	private bool _status = false;
-	private Machine Machines = [];
+	/* Machine是Button的listener */
+	private List<ButtonListener> _listenerList = new List<ButtonListener>();
+	
+	private bool _startGoingDown = false;
+	private float _startTime;
+	private float _fromY;
+	private float _targetY = .175f;
+	private Vector3 _newPos;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		_fromY = transform.position.y;
+		_newPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//按钮下降
+		if(_startGoingDown) {
+			float linearProportion = (Time.time - _startTime) / moveDuration;
+			float newY = Mathf.SmoothStep(_fromY, _targetY, linearProportion);
+			// Debug.Log(newY+" "+_fromY+" "+_targetY);
+			if(newY != _targetY) {
+				_newPos.y = newY;
+				transform.position = _newPos;
+			} else {/* 下降结束 */
+				_startGoingDown = false;
+				_triggerListeners();
+			}
+		}
 	}
 
-	//当被碰到，就会触发状态变化
-	void OnCollisionEnter(Collision collision) {
-		_status = !_status;
+	//当被碰到，就会变成打开
+	void OnCollisionEnter() {
+		if(_status == false) {
+			_status = !_status;
 
+			//开始下降
+			_startGoingDown = true;
+			_startTime = Time.time;
+		}
+	}
+
+	/* 触发监听器 */
+	private void _triggerListeners() {
+		foreach(ButtonListener listener in _listenerList) {
+			listener.onButtonChange(_status);
+		}
+	}
+
+	public void addButtonListener(ButtonListener listener) {
+		_listenerList.Add(listener);
 	}
 }
---
+++ Assets/scripts/LazerBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerBase : ButtonListener {
+	public GameObject lazerGO;
+	//如果连接到按钮，就跟下面的状态无关，而是听按钮的话
+	// public Button controlButton = null;
+	//如果不链接到按钮，就保持这个状态
+	public bool staticStatus = true;
+
+	private bool _status = false;
+	private GameObject _lazerGO;
+	//要保证它的长度正好是1个单位（粗细不用是1个单位）,且那个方向的scale也是1
+	private Vector3 _lazerScale;
+
+	// Use this for initialization
+	void Start () {
+		_lazerGO = Instantiate(lazerGO);
+		_lazerScale = _lazerGO.transform.localScale;
+		_lazerGO.transform.position = this.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_status) {
+			Debug.DrawRay(transform.position, transform.forward);
+			RaycastHit hit;
+			//射到了
+			if(Physics.Raycast(transform.position, transform.forward, out hit)) {
+				_lazerScale.z = hit.distance;
+			} else { //没射到
+				_lazerScale.z = 30;
+			}
+			_lazerGO.transform.localScale = _lazerScale;
+		}
+	}
+
+	override public void onButtonChange(bool status) {
+		_status = status;
+		Debug.Log("status");
+	}
+
+	override public void ifNoButton() {
+		_status = staticStatus;
+	}
+
+}
